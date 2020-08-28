3d77f7650dcdb38c5ce0a69435b334ab7133da63
mathew94 <mathewrodrick94@hotmail.com>
2020-08-28 16:27:14 +0600

    full complete

--- src/Components/Course/Course.js
+++ src/Components/Course/Course.js
@@ -5,8 +5,6 @@ import './Course.css';
 import Product from './../Product/Product';
 
 const Course = () => {
-    const course15 = courseData.slice(0,15);
-    const [products, setProducts] = useState(course15);
     const [cart, setCart] = useState([]);
 
     const handleAddCourse = (product) => {
@@ -18,7 +16,7 @@ const Course = () => {
         <div className="whole-container">
             <div className="product-container">
             {
-                products.map( pd => <Product handleAddCourse = {handleAddCourse} product={pd} key = {pd.courseId}></Product>)
+                courseData.map( pd => <Product handleAddCourse = {handleAddCourse} product={pd} key = {pd.courseId}></Product>)
             }   
             </div>
             <div className="cart-container">
